726b1ecb4033d3011c34f7b3a9d205ad295ac477
Thiamath <thiamath@gmail.com>
2020-03-05 12:36:21 +0100

    Set the Spring Boot environment bootstrap.

---
+++ src/test/java/com/thiamath/lmassignement/LasssignementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiamath.lmassignement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LasssignementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
